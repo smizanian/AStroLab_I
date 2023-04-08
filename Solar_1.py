a9f662112567a4e769e92d6f5e680d66d133000e
smizanian <95073227+smizanian@users.noreply.github.com>
2023-04-08 20:22:47 +0330

    RMS added

--- Solar_1.py
+++ Solar_1.py
@@ -7,13 +7,31 @@ import numpy as np
 
 def master_image(images: List[np.ndarray]) -> np.ndarray:
     stacked_array = np.stack(images)
-    return np.mean(stacked_array, axis=0)
+    return np.median(stacked_array, axis=0)
 
 def normalize_array(a):
     return a/np.max(a)
 
 def normalize_flat(a):
     return a/np.median(a)
+
+def image_bin(a, y_c, index):
+    sum_flux = 0
+    count = 0
+    for i in range(-3,4):
+        for j in range(-3, 4):
+            sum_flux+=a[16][y_c+j][index+i]
+            count = count+1
+    return sum_flux/count
+
+def flux_rms(a, y_c, index):
+    sum_flux = 0
+    count = 0
+    for i in range(-10,11):
+        for j in range(-10, 11):
+            sum_flux+=(a[16][y_c+j][index+i])**2
+            count = count+1
+    return sum_flux//count 
 
 sun_radius = 30
 
@@ -46,16 +64,12 @@ images.append((images[15] - master_image(images[5:9]))/(normalize_flat(master_im
 
 print(len(images[16][0]))
 for i in range(len(images[16][0])):
-    #print(images[15][0][i])
     if(images[16][0][i]>sun_radius):
-        #print(i)
         start_h = i
         break;
 
 for i in range(len(images[16][0])):
-    #print(images[15][0][i])
     if(images[16][0][3905-i]>sun_radius):
-        #print(i)
         end_v = i
         break;
 
@@ -64,9 +78,7 @@ start_v = 0
 end_v = 0
 print(len(images[16]))
 for i in range(len(images[16])):
-    #print(images[15][0][i])
     if(images[16][i][3905]>sun_radius):
-        #print(i)
         start_v = i
         break;
 
@@ -88,21 +100,23 @@ radius = 0
 for i in range(x_c):
     if(images[16][y_c][i]>sun_radius and radius==0):
         radius = i
-    if(i%100 == 0):
+
+radius = x_c - radius
+print(radius)
+
+for i in range(x_c):
+    if(i%100 == 0 and (x_c - i) < radius):
         sun_data_x.append(x_c - i)
-        sun_data_y.append(images[16][y_c][i])
-        print(images[16][y_c][i])
+        sun_data_y.append(image_bin(images, y_c, i))
+        print(image_bin(images, y_c, i))
 
 sun_data_y = normalize_array(sun_data_y)
 
 print("Plot: data of sun")
-plt.plot(sun_data_x, sun_data_y)
+plt.plot(sun_data_x, sun_data_y, marker='o')
 plt.xlabel("radius (pixel)")
 plt.ylabel("normalized flux")
 plt.show()
-
-radius = x_c - radius
-print(radius)
 
 print("Plot: mu")
 
@@ -116,16 +130,32 @@ for i in range(x_c):
     if(i%100 == 0 and (x_c - i) < radius):
         mu = (1-((x_c - i)/radius)**2)**0.5
         mu_data_x.append(mu)
-        mu_data_y.append(images[16][y_c][i])
+        mu_data_y.append(image_bin(images, y_c, i))
         edd_data_y.append((2+3*mu)/5)
-        print(images[16][y_c][i])
+        print(image_bin(images, y_c, i))
 
 mu_data_y = normalize_array(mu_data_y)
 
-plt.plot(mu_data_x, mu_data_y, color='g', label='solar data')
+plt.plot(mu_data_x, mu_data_y, color='g', label='solar data', marker='o')
 plt.plot(mu_data_x, edd_data_y, color='r', label='eddington modle')
 plt.xlabel("mu")
 plt.ylabel("normalized flux")
+plt.show()
+
+print("flux RMS")
+
+rms_data_x = []
+rms_data_y = []
+
+for i in range(x_c):
+    if(i%100 == 0 and (x_c - i) < radius):
+        rms_data_x.append(x_c - i - 2)
+        rms_data_y.append(flux_rms(images, y_c, i))
+        print(flux_rms(images, y_c, i))
+        
+plt.plot(rms_data_x, rms_data_y, marker='o')
+plt.xlabel("radius (pixel)")
+plt.ylabel("flux rms")
 plt.show()
 
     
